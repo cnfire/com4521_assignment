4d77f260b2594d504c70c3de144db01b5de95dd4
XIAOWEI <XIAOWEI@EC2AMAZ-VPRNJV1>
2020-04-14 22:58:23 +0000

    from remote

--- NBody/NBody.c
+++ NBody/NBody.c
@@ -171,6 +171,7 @@ int main(int argc, char* argv[]) {
 	//argv is an array (of length argc) of the arguments. The first argument is always the executable name (including path)
 	parse_parameter(argc, argv);
 
+
 	//TODO: Allocate any heap memory
 //    nbody arr[N];
 	bodies = (nbody*)malloc(N * sizeof(nbody));
@@ -192,7 +193,18 @@ int main(int argc, char* argv[]) {
 
 		//TODO: Depending on program arguments, either configure and start the visualiser or perform a fixed number of simulation steps (then output the timing results).
 	if (M == CPU) {
-		// step();
+		//for (int i = 0; i < I; i++) {
+		//	printf("\niteration epoch:%d", i);
+		//	//start timer
+		//	double begin = omp_get_wtime();
+		//	step();
+		//	//stop timer
+		//	double end = omp_get_wtime();
+		//	double elapsed = end - begin;
+		//	printf("\nComplete in %f seconds\n", elapsed);
+		//}
+
+
 		initViewer(N, D, M, step);
 		setNBodyPositions(bodies);
 		//setActivityMapData(density);
@@ -213,79 +225,69 @@ void step(void) {
 	//TODO: Perform the main simulation of the NBody system
 	printf("\ncompute by serial");
 	vector* f_arr = (vector*)malloc(N * sizeof(vector));
-
-	for (int i = 0; i < I; i++) {
-		//start timer
-		double begin = omp_get_wtime();
-		printf("\niteration epoch:%d", i);
-		//        vector f_arr[N];
-		for (int l = 0; l < N; l++) {
-			nbody* body_l = &bodies[l];
-			vector f = { 0, 0 };
-			for (int j = 0; j < N; j++) {
-				// skip the influence of body on itself
-				if (j == l) continue;
-				nbody* body_j = &bodies[j];
-				vector s1 = { body_j->x - body_l->x, body_j->y - body_l->y };
-				vector s2 = { s1.x * body_j->m, s1.y * body_j->m };
-				double s3 = pow(pow(s1.x, 2) + pow(s1.y, 2) + pow(SOFTENING, 2), 1.5);
-				f.x = (float)(f.x + s2.x / s3);
-				f.y = (float)(f.y + s2.y / s3);
-			}
-			f.x = G * body_l->m * f.x;
-			f.y = G * body_l->m * f.y;
-			//                printf("\nvector %d = (%f,%f)", j, f.x, f.y);
-			f_arr[l].x = f.x;
-			f_arr[l].y = f.y;
-		}
-		//        print_bodies();
-
-				//calculate the values of the D*D locations (density)
-				// reset the value to zero
-		for (int j = 0; j < D * D; j++) {
-			density[j] = 0.0f;
-		}
+	//        vector f_arr[N];
+	for (int l = 0; l < N; l++) {
+		nbody* body_l = &bodies[l];
+		vector f = { 0, 0 };
 		for (int j = 0; j < N; j++) {
-			nbody* body = &bodies[j];
-			double scale = 1.0 / D;
-			// x-axis coordinate of D*D locations
-			int x = (int)ceil(body->x / scale) - 1;
-			// y-axis coordinate of D*D locations
-			int y = (int)ceil(body->y / scale) - 1;
-			// the index of one dimensional array
-			int index = x * D + (y + 1);
-			// For large values of N
-			int a = N;
-			if (N > LARGER_N) {
-				a = N * D;
-			}
-			density[index] = (density[index] * a + 1) / a;
-			//            density[index] = density[index] + 1;
-			//            printf("\nindex:%d: density:%f", index, density[index]);
+			// skip the influence of body on itself
+			if (j == l) continue;
+			nbody* body_j = &bodies[j];
+			vector s1 = { body_j->x - body_l->x, body_j->y - body_l->y };
+			vector s2 = { s1.x * body_j->m, s1.y * body_j->m };
+			double s3 = pow(pow(s1.x, 2) + pow(s1.y, 2) + pow(SOFTENING, 2), 1.5);
+			f.x = (float)(f.x + s2.x / s3);
+			f.y = (float)(f.y + s2.y / s3);
 		}
-		//        for (int j = 0; j < D * D; j++) {
-		//            printf("\nindex:%d: density:%f", j, density[j]);
-		//        }
-
-				// update location,velocity,
-		for (int j = 0; j < N; j++) {
-			nbody* body = &bodies[j];
-			vector a = { f_arr[j].x / body->m, f_arr[j].y / body->m };
-			// new velocity
-			vector v_new = { body->vx + dt * a.x, body->vy + dt * a.y };
-			// new location
-			vector l_new = { body->x + dt * v_new.x, body->y + dt * v_new.y };
-			body->x = l_new.x;
-			body->y = l_new.y;
-			body->vx = v_new.x;
-			body->vy = v_new.y;
-		}
-		//stop timer
-		double end = omp_get_wtime();
-
-		double elapsed = end - begin;
-		printf("\nComplete in %f seconds\n", elapsed);
+		f.x = G * body_l->m * f.x;
+		f.y = G * body_l->m * f.y;
+		//                printf("\nvector %d = (%f,%f)", j, f.x, f.y);
+		f_arr[l].x = f.x;
+		f_arr[l].y = f.y;
 	}
+	//        print_bodies();
+
+			//calculate the values of the D*D locations (density)
+			// reset the value to zero
+	for (int j = 0; j < D * D; j++) {
+		density[j] = 0.0f;
+	}
+	for (int j = 0; j < N; j++) {
+		nbody* body = &bodies[j];
+		double scale = 1.0 / D;
+		// x-axis coordinate of D*D locations
+		int x = (int)ceil(body->x / scale) - 1;
+		// y-axis coordinate of D*D locations
+		int y = (int)ceil(body->y / scale) - 1;
+		// the index of one dimensional array
+		int index = x * D + (y + 1);
+		// For large values of N
+		int a = N;
+		if (N > LARGER_N) {
+			a = N * D;
+		}
+		density[index] = (density[index] * a + 1) / a;
+		//            density[index] = density[index] + 1;
+		//            printf("\nindex:%d: density:%f", index, density[index]);
+	}
+	//        for (int j = 0; j < D * D; j++) {
+	//            printf("\nindex:%d: density:%f", j, density[j]);
+	//        }
+
+			// update location,velocity,
+	for (int j = 0; j < N; j++) {
+		nbody* body = &bodies[j];
+		vector a = { f_arr[j].x / body->m, f_arr[j].y / body->m };
+		// new velocity
+		vector v_new = { body->vx + dt * a.x, body->vy + dt * a.y };
+		// new location
+		vector l_new = { body->x + dt * v_new.x, body->y + dt * v_new.y };
+		body->x = l_new.x;
+		body->y = l_new.y;
+		body->vx = v_new.x;
+		body->vy = v_new.y;
+	}
+
 }
 
 void step_with_parallel(void) {
